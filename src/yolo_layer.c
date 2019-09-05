1e234ab2a75551574586678b3bcc7dbc6ecf8058
sola <opop0628@gmail.com>
2019-09-05 16:56:35 +0900

    deep part1 debug

--- src/yolo_layer.c
+++ src/yolo_layer.c
@@ -246,7 +246,8 @@ void forward_yolo_layer(const layer l, network_state state)
                         box truth = float_to_box_stride(state.truth + t*(4 + 1) + b*l.truths, 1);
                         int class_id = state.truth[t*(4 + 1) + b*l.truths + 4];
                         //if (class_id >= l.classes) {
-                        if ( (!l.part && class_id >= l.whole_classes) || 
+                        if ( (!l.part && !l.whole && class_id >= l.classes) || 
+                            (!l.part && class_id >= l.whole_classes) || 
                             (l.part && (class_id < l.whole_classes || class_id >= l.classes + l.whole_classes )) ) {
                             //printf(" Warning: in txt-labels class_id=%d >= classes=%d in cfg-file. In txt-labels class_id should be [from 0 to %d] \n", class_id, l.classes, l.classes - 1);
                             //getchar();
@@ -282,7 +283,8 @@ void forward_yolo_layer(const layer l, network_state state)
             box truth = float_to_box_stride(state.truth + t*(4 + 1) + b*l.truths, 1);
             int class_id = state.truth[t*(4 + 1) + b*l.truths + 4];
             //if (class_id >= l.classes) continue; // if label contains class_id more than number of classes in the cfg-file
-            if ( (!l.part && class_id >= l.whole_classes) || 
+            if ( (!l.part && !l.whole && class_id >= l.classes) || 
+                (!l.part && class_id >= l.whole_classes) || 
                 (l.part && (class_id < l.whole_classes || class_id >= l.classes + l.whole_classes )) ) {
                 continue;
             }
